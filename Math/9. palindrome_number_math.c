656f9116494a5880a33f114b82b10b4486d3059b
Prajwal SM <smprajwal2005@gmail.com>
2025-11-04 20:13:50 +0530

    Add 9. Palindrome Number problem (Math solution)

---
+++ Math/9. palindrome_number_math.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+#include<stdlib.h>
+bool isPalindrome(int x) {
+    // Negative numbers are not palindrome
+    if (x < 0 || (x % 10 == 0 && x != 0))
+        return false;
+
+    int reversedHalf = 0;
+    while (x > reversedHalf) {
+        reversedHalf = reversedHalf * 10 + x % 10;
+        x /= 10;
+    }
+
+    // For even-length numbers, x == reversedHalf
+    // For odd-length, x == reversedHalf/10 (middle digit ignored)
+    return (x == reversedHalf || x == reversedHalf / 10);
+}
